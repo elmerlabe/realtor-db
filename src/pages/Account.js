6abf4b71071ea1d89c78e3d3a22df812e4b3c519
elmer <elmerlabe@gmail.com>
2023-05-05 14:28:34 +0800

    adjust width

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -51,7 +51,7 @@ const Account = () => {
       <div>
         <div>
           <h1 className="mb-4 text-2xl font-bold text-gray-900">
-            Security Settings
+            Account Settings
           </h1>
           <form onSubmit={handleFormSecurSubmit}>
             <div className="md:w-1/3 grid md:grid-cols-1 bg-white p-6 shadow rounded-md">
@@ -157,7 +157,7 @@ const Account = () => {
               <div>
                 <button
                   type="submit"
-                  className="mr-4 md:w-30 tracking-widest items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                  className="mr-4 md:w-full tracking-widest items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                 >
                   Update
                 </button>
