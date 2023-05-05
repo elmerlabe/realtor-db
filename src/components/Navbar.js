bd7ecb1983562f5d4d0268676a2eb8d88cd9ad1d
bowkerdev <dev.bowkerideas@gmail.com>
2023-05-05 14:13:18 +0800

    adjust max-width & padding

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -49,15 +49,13 @@ export default function Navbar({ children }) {
         <Disclosure as="nav" className="bg-gray-800">
           {({ open }) => (
             <>
-              <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+              <div className="mx-auto max-w-8xl px-4 sm:px-6 lg:px-20">
                 <div className="flex h-16 items-center justify-between">
                   <div className="flex items-center">
                     <div className="flex-shrink-0">
                       <CircleStackIcon className="w-6 h-6 text-white" />
                     </div>
-                    <span className="text-white ml-2 font-bold">
-                      Realtor DB
-                    </span>
+                    <span className="text-white ml-2 font-bold">RealtorDB</span>
                     <div className="hidden md:block">
                       <div className="ml-10 flex items-baseline space-x-4">
                         {navigation.map((item) => (
@@ -208,7 +206,7 @@ export default function Navbar({ children }) {
         </Disclosure>
 
         <main>
-          <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+          <div className="mx-auto max-w-8xl py-6 sm:px-6 lg:px-20">
             {children}
           </div>
         </main>
